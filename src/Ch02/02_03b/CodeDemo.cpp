353bd37ccf8035b2a47edcda9902d2d73f429402
rc <91196618+rahul334455@users.noreply.github.com>
2025-05-13 13:25:47 +0000

    3 intger created

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,29 @@
 
 #include <iostream>
 
+int a, b = 5; // single line comment
+
+/*
+multi 
+line 
+comment
+*/
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool  my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " <<my_flag << std::endl;
+    my_flag = true;
+    std:: cout << "my_flag = " << my_flag << std::endl;
+    std:: cout << "a+b = " << a+b << std::endl;
+    std:: cout << "a-b = " << b-a << std::endl; 
+    unsigned int positive;
+    positive = b-a;
+
+    std::cout << "b-a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
