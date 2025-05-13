d71049c38e218169872c90a98f9910f921b5a29f
rc <91196618+rahul334455@users.noreply.github.com>
2025-05-13 21:26:03 +0000

    typecasting exmp

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,14 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+    std::cout << "unsgn is " << (int32_t)unsgn << std::endl;  
     std::cout << std::endl << std::endl;
     return (0);
 }
