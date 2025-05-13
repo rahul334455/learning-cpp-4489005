8f0d87bb134408f89ea3607d925c617b8ccd98f5
rc <91196618+rahul334455@users.noreply.github.com>
2025-05-13 13:51:08 +0000

    added typrinfo  code

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -5,7 +5,23 @@
 #include <iostream>
 #include <typeinfo>
 
+
 int main(){
+    auto a = 8;
+    auto b = 12345678901;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'd';
+    auto g = "C++ rocks";
+
+    std:: cout << "the type of a is " << typeid(a).name() << std::endl;
+    std:: cout << "the type of b is " << typeid(b).name() << std::endl;
+    std:: cout << "the type of c is " << typeid(c).name() << std::endl;
+    std:: cout << "the type of d is " << typeid(d).name() << std::endl;
+    std:: cout << "the type of e is " << typeid(e).name() << std::endl;
+    std:: cout << "the type of f is " << typeid(f).name() << std::endl;
+    std:: cout << "the type of g is " << typeid(g).name() << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
